08dfbf0fa171b6c4854b3a0fa09d002b717d0272
markplotlib <mark.chesney@gmail.com>
2020-09-07 20:01:20 -0700

    do another fix (+4 squashed commit)
    Squashed commit:
    
    [8e99b8c] fix ignores
    
    [0c95f68] add correct path to gitignore
    
    [863f5df] update gitignore
    
    [f1d2207] drop and create tables

--- sql_queries.py
+++ sql_queries.py
@@ -1,52 +1,31 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP table songplays"
+user_table_drop = "DROP table users"
+song_table_drop = "DROP table songs"
+artist_table_drop = "DROP table artists"
+time_table_drop = "DROP table time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
-""")
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id int, start_time timestamp, 
+                            user_id int, level int, song_id int, artist_id int, 
+                            session_id int, location varchar , user_agent varchar);""")
 
-user_table_create = ("""
-""")
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id int, first_name varchar, last_name varchar, 
+                        gender varchar, level int);""")
 
-song_table_create = ("""
-""")
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id int, title varchar, artist_id int, 
+                        year int, duration time);""")
 
-artist_table_create = ("""
-""")
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id int, name varchar, location varchar, 
+                          latitude decimal, longitude decimal);""")
 
-time_table_create = ("""
-""")
-
-# INSERT RECORDS
-
-songplay_table_insert = ("""
-""")
-
-user_table_insert = ("""
-""")
-
-song_table_insert = ("""
-""")
-
-artist_table_insert = ("""
-""")
-
-
-time_table_insert = ("""
-""")
-
-# FIND SONGS
-
-song_select = ("""
-""")
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time timestamp, hour int, day int, week int, 
+                        month int, year int, weekday bool);""")
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create,
+                        time_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
